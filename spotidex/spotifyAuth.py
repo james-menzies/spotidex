d12ceb0af0fae16707e32ba0a7ce82915f29875d
James Menzies <james.r.menzies@gmail.com>
2020-09-21 16:23:57 +1000

    test cases added to spAuth, more needed

--- spotidex/spotifyAuth.py
+++ spotidex/spotifyAuth.py
@@ -1,11 +1,13 @@
 from spotipy import Spotify
-from spotipy.oauth2 import SpotifyPKCE, SpotifyOauthError
+from spotipy.oauth2 import SpotifyPKCE, SpotifyOauthError, SpotifyException
 import os
+import json
 
 
 class SpotifyAuth:
 
     __instance = None
+    __cache_path = "test/resources/valid_cache"
 
     @staticmethod
     def getInstance():
@@ -16,7 +18,11 @@ class SpotifyAuth:
     def __init__(self):
         if SpotifyAuth.__instance:
             raise ValueError("There can only be one instance of SpotifyAuth")
-        self.__current_user = None
+        
+        self.__pkce = self.__init__pkce()
+        self.__endpoint = None
+        self.__current_user = self.__init_current_user()
+        
 
     def __init__pkce(self) -> SpotifyPKCE:
         scope = "user-read-currently-playing"
@@ -24,12 +30,18 @@ class SpotifyAuth:
         redirect_uri = "http://localhost:8080/"
         
         return SpotifyPKCE(
-            client_id=client_id, redirect_uri=redirect_uri, scope=scope)
+            client_id=client_id, redirect_uri=redirect_uri, scope=scope,cache_path=SpotifyAuth.__cache_path )
 
-    @property
-    def logged_in(self) -> bool:
-        return self.__logged_in
-    
+
+    def __init_current_user(self) -> str:
+        cache_path = self.__pkce.cache_path
+        if os.path.exists(cache_path):
+            with open(cache_path, "r") as cache_file:
+                cache_data = json.load(cache_file)
+                if "logged_in_user" in cache_data:
+                    return cache_data["logged_in_user"]
+
+
     @property
     def current_user(self) -> str:
         return self.__current_user
@@ -37,14 +49,38 @@ class SpotifyAuth:
     @property
     def endpoint(self) -> Spotify:
         if not self.__endpoint:
-            self.__endpoint = Spotify(auth_manager=self.__pkce)
-            
-            try:
-                self.__current_user = self.__endpoint.current_user()
-            except SpotifyOauthError:
-                print("User authorization failed.")
-                self.__endpoint = None
-                return None
+            self.__logged_in = self.establish_connection()
         
         self.__logged_in = True
         return self.__endpoint
+
+
+    def establish_connection(self):
+        self.__endpoint = Spotify(auth_manager=self.__pkce)
+        validated = False
+        while not validated:
+            try:
+                self.__current_user = self.__endpoint.current_user()["display_name"]
+                validated = True
+                self.update_user_in_cache(self.__current_user)
+              
+            except (SpotifyOauthError, KeyboardInterrupt):
+                # user fails or cancels login
+                print("User authorization failed.")
+                self.__endpoint = None
+                return None
+            except:
+                print("Spotify auth token cache was invalid")
+                os.remove(self.__pkce.cache_path)
+    
+
+    def update_user_in_cache(self, current_user: str):
+
+        cache_path = self.__pkce.cache_path
+
+        with open(cache_path, "r") as cache_file:
+            cache_data = json.load(cache_file)
+            cache_data["logged_in_user"] = current_user
+        
+        with open(cache_path, "w") as cache_file:
+            json.dump(cache_data, cache_file)
--- test/test_spotifyAuth.py
+++ test/test_spotifyAuth.py
@@ -1,9 +1,24 @@
 import unittest
-from spotidex import spotifyAuth
+from spotidex.spotifyAuth import SpotifyAuth
 
 class test_example(unittest.TestCase):
-    
-    def test_pass(self):
-        self.assertEqual(1, 1)
 
+    def setUp(self):
+        SpotifyAuth._SpotifyAuth__instance = None
+        SpotifyAuth._SpotifyAuth__cache_path = "invalid/file/path"
     
+    def test_user_exists_valid_cache(self):
+        SpotifyAuth._SpotifyAuth__cache_path = "test/resources/valid_cache"
+        auth = SpotifyAuth.getInstance()        
+        self.assertEqual("redbrickhut", auth.current_user, "user redbrickhut should be logged in")
+
+    def test_no_cache_no_current_user(self):
+        auth = SpotifyAuth.getInstance()
+        self.assertIsNone(auth.current_user, "no cache should mean that there is no current user")
+
+    def test_invalid_cache_fails(self):
+        SpotifyAuth._SpotifyAuth__cache_path = "test/resources/invalid_cache"
+        auth = SpotifyAuth.getInstance()
+        # TODO write remainder        
+
+
