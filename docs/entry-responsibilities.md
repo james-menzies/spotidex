086166c48e1dfcc52da7188b4b8a7612f9ef5481
James Menzies <james.r.menzies@gmail.com>
2020-09-27 11:19:21 +1000

    working prototype for entry, scroll.py dependency added

--- docs/entry-responsibilities.md
+++ docs/entry-responsibilities.md
@@ -4,4 +4,18 @@ An Entry-VM manages the entire main display of the app.
 
 When it is loaded, it:
 
-Looks at the settings to determine which views are being displayed.
+Looks at the settings to determine which views are being displayed. It has a record of all the dependencies for each view.
+
+It then instantiates an instance of all of these views.
+
+It then loads the current song and exposes that variable.
+
+
+
+The view will call the get_views method, and the entry_vm will return them.
+
+The entry will provide an update method, which will
+
+
+It then queries the session to see if there was a previous match, if so, it loads it in and makes sure that all of its dependencies are loaded.
+
--- spotidex/views/entry.py
+++ spotidex/views/entry.py
@@ -1,16 +1,33 @@
 import urwid
 
-from spotidex.viewmodels.entry_vm import EntryVM
+from .scroll import Scrollable
 
 
 class Entry:
     pass
     
     def __init__(self):
-        self.vm = EntryVM()
-        txt = urwid.Text(self.vm.current)
-        self.__widget = urwid.Filler(txt)
+        pass
     
     @property
     def widget(self):
-        return self.__widget
+        text = """On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
+        
+        On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        
+        On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        """
+        text = [urwid.Text(line) for line in text.splitlines()]
+        captions = "ComposerView ClassicalView WorkView BasicInfo".split()
+        items = [urwid.Button(caption) for caption in captions]
+        walker = urwid.SimpleListWalker(items)
+        gridflow = urwid.GridFlow(walker, cell_width=30, h_sep=1, v_sep=1, align='center')
+        
+        info_pile = urwid.Pile(text)
+        
+        scrollable = urwid.BoxAdapter(Scrollable(info_pile), 40)
+        info_view = urwid.LineBox(scrollable, title="Scrollable Demo")
+        
+        pile = urwid.Pile([gridflow, info_view], gridflow)
+        filler = urwid.Filler(pile)
+        return filler
