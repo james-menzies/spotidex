95e4ebf25063589931fb9db0d46983eefe080b9e
James Menzies <james.r.menzies@gmail.com>
2020-09-27 15:06:15 +1000

    prototype now fully functional, can dynamically update contexts

--- docs/entry-responsibilities.md
+++ docs/entry-responsibilities.md
@@ -4,18 +4,9 @@ An Entry-VM manages the entire main display of the app.
 
 When it is loaded, it:
 
-Looks at the settings to determine which views are being displayed. It has a record of all the dependencies for each view.
+Looks at the settings to determine which views are being displayed. It has a record of all the dependencies for each view. It instantiates an instance of each view.
 
-It then instantiates an instance of all of these views.
+The view will query the viewmodel for the views.
 
-It then loads the current song and exposes that variable.
-
-
-
-The view will call the get_views method, and the entry_vm will return them.
-
-The entry will provide an update method, which will
-
-
-It then queries the session to see if there was a previous match, if so, it loads it in and makes sure that all of its dependencies are loaded.
+The viewmodel will then provide a method for the view to return the data dictionary for the current track. If the information has not changed since the last request, it returns None instead.
 
--- spotidex/views/entry.py
+++ spotidex/views/entry.py
@@ -17,17 +17,30 @@ class Entry:
         
         On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
         """
+        
+        widgets = {}
         text = [urwid.Text(line) for line in text.splitlines()]
         captions = "ComposerView ClassicalView WorkView BasicInfo".split()
-        items = [urwid.Button(caption) for caption in captions]
-        walker = urwid.SimpleListWalker(items)
-        gridflow = urwid.GridFlow(walker, cell_width=30, h_sep=1, v_sep=1, align='center')
         
-        info_pile = urwid.Pile(text)
+        items = [urwid.LineBox(urwid.Button(caption)) for caption in captions]
         
-        scrollable = urwid.BoxAdapter(Scrollable(info_pile), 40)
-        info_view = urwid.LineBox(scrollable, title="Scrollable Demo")
+        walker = urwid.SimpleFocusListWalker(items)
+        grid_flow = urwid.GridFlow(walker, cell_width=25, h_sep=1, v_sep=1, align='center')
         
-        pile = urwid.Pile([gridflow, info_view], gridflow)
-        filler = urwid.Filler(pile)
-        return filler
+        def callback(button: urwid.Button, index: int):
+            frame.contents['body'] = (widgets[index], None)
+        
+        for index, caption in enumerate(captions):
+            body = text.copy()
+            body = [urwid.Text(caption), urwid.Divider(div_char='-')] + body
+            info_pile = urwid.Pile(body)
+            scrollable = Scrollable(info_pile)
+            widgets[index] = scrollable
+            urwid.connect_signal(items[index].original_widget, "click", callback, user_arg=index)
+
+        frame = urwid.Frame(widgets[0])
+        box = urwid.LineBox(frame)
+        box = urwid.BoxAdapter(box, 25)
+        pile = urwid.Pile([grid_flow, box])
+        return urwid.Filler(pile)
+       
