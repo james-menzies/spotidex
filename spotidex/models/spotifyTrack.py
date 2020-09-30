f58458ed7d97f691bf47da12ba5a6eb8af5b8e99
James Menzies <james.r.menzies@gmail.com>
2020-09-30 14:59:22 +1000

    count down latch implemented in spotify track. song retrieval faster

--- spotidex/models/spotifyTrack.py
+++ spotidex/models/spotifyTrack.py
@@ -1,14 +1,32 @@
+import threading
 from typing import List, Optional
 
 from .context import *
+
+
+class CountDownLatch(object):
+    def __init__(self, count=1):
+        self.count = count
+        self.lock = threading.Condition()
+    
+    def count_down(self):
+        self.lock.acquire()
+        self.count -= 1
+        if self.count <= 0:
+            self.lock.notifyAll()
+        self.lock.release()
+    
+    def wait_on(self):
+        self.lock.acquire()
+        while self.count > 0:
+            self.lock.wait()
+        self.lock.release()
 
 
 class SpotifyTrack:
     
     def __init__(self, raw_data: str, contexts: Optional[List[Context]] = None):
         
-        if contexts is None:
-            contexts = []
         if not raw_data:
             self.__information = None
             return
@@ -17,12 +35,29 @@ class SpotifyTrack:
                 "raw_data": raw_data
             }
         
-        contexts = [BasicInfo, ComposerInfo, ClassicalInfo, RecommendedInfo, ComposerWikiInfo, WorkWikiInfo]
+        basic_contexts = [BasicInfo, ComposerInfo, ClassicalInfo]
+        dependency_contexts = [RecommendedInfo, ComposerWikiInfo, WorkWikiInfo]
+        lock = threading.Lock()
         
-        for context in contexts:
+        def get_context(context, latch: Optional[CountDownLatch] = None):
             info = context().fetch(self.__information)
             if info:
+                lock.acquire()
                 self.__information.update(info)
+                lock.release()
+            if latch:
+                latch.count_down()
+        
+        for context in basic_contexts:
+            get_context(context)
+        
+        latch = CountDownLatch(len(dependency_contexts))
+        
+        for context in dependency_contexts:
+            thread = threading.Thread(target=get_context, args=(context, latch))
+            thread.start()
+        
+        latch.wait_on()
     
     def __eq__(self, other) -> bool:
         
