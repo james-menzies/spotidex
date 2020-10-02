e3c43cbd92462712429f5ecdd93c0261b15be914
James Menzies <james.r.menzies@gmail.com>
2020-10-02 15:51:56 +1000

    setup.py and main errors hopefully corrected. About to test

--- spotidex/__main__.py
+++ spotidex/__main__.py
@@ -1,24 +1,35 @@
 import logging
-import sys
 import os
+import sys
+import pathlib
 
 from spotidex.models.spotifyAuth import SpotifyAuth
 from spotidex.views.login_screen import LoginScreen
 from spotidex.views.main_menu import MainMenu
 from spotidex.views.terminal_wrapper import TerminalWrapper
 
-logging.disable(sys.maxsize)
-sys.stderr = open(os.devnull, "w")
-sys.stdout = open(os.devnull, "w")
 
-auth = SpotifyAuth.get_instance()
+def main(args=None):
+    
+    logging.disable(sys.maxsize)
+    sys.stderr = open(os.devnull, "w")
+    sys.stdout = open(os.devnull, "w")
+    os.environ["SPOTIPY_CLIENT_ID"] = "711661c6916a4a9981244380aa852adc"
+    sys.path.insert(0, str(pathlib.Path(__file__).parents[1]))
+    
+    
+    auth = SpotifyAuth.get_instance()
+    
+    if auth.current_user:
+        starting_screen = MainMenu()
+    else:
+        starting_screen = LoginScreen()
+    
+    try:
+        TerminalWrapper.start_application(starting_screen)
+    except KeyboardInterrupt:
+        TerminalWrapper.clean_resources()
 
-if auth.current_user:
-    starting_screen = MainMenu()
-else:
-    starting_screen = LoginScreen()
 
-try:
-    TerminalWrapper.start_application(starting_screen)
-except KeyboardInterrupt:
-    TerminalWrapper.clean_resources()
+if __name__ == "__main__":
+    sys.exit(main())
